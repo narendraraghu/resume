cb958aa05331107d8357d9888ad9ea6d85b194a6
narendraraghu <narendrakareli@yahoo.com>
2021-02-28 23:20:49 +0530

    resume changes

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+  "imagebaseurl":"http://narendraraghu.github.io/resume",
+  "name": "Narendra Raghuwanshi",
+  "role": "Full Stack Developer",
+  "linkedinId":"https://www.linkedin.com/in/narendraraghu",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/narendraraghu",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/narendraraghu",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com/narendrakareli",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I have finished my B.E. in Information Technology and have been working in industry for past few years.",
+  "address":"India",
+  "website":"https://narendraraghu.github.io/",
+  "education":[
+    {
+      "UniversityName":"RGPV University",
+      "specialization":"Information Technology",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2012",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"CDAC",
+      "specialization":"Advance Computing",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2014",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"ThermoFisher Scientific",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"Java"
+    },
+    {
+      "skillname":"AWS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
